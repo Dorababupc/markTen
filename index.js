ff31cb093355de7f715962c97c485fb32c5a8497
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2021-12-08 18:33:36 +0530

    basic structure and wiring.

---
+++ index.js
@@ -0,0 +1,4 @@
+const billAmount=document.querySelector("#bill-amount");
+var cashGiven=document.querySelector("#cash-given");
+var checkButton=document.querySelector("#check-button");
+checkButton.addEventListener("click",()=>console.log("clicked"));
